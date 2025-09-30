8828f28b5d28e8fc9b2b04a11c37adef33a9a85d
Carlos Vera <vera.0214@outlook.com>
2025-09-30 17:43:47 -0600

    Funny Story

---
+++ assignment1.js
@@ -0,0 +1,15 @@
+
+let hero = "Max";
+let animal = "dragon";
+let action = "sneezed fire";
+let place = "on top of the mountain";
+let time = "a cold winter night";
+
+
+const twist = "a secret treasure chest";
+
+
+console.log("─── Funny Adventure ───");
+console.log(hero + " was with a " + animal + " " + place + " on " + time + ".");
+console.log("Suddenly, the " + animal + " " + action + " and everything turned chaotic!");
+console.log("It turned out to reveal " + twist + " and everyone was shocked!");
